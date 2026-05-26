fdb99276220c604a82a20dea7abc306a0eae92b9
Lean 4 VS Code Extension <>
2026-05-26 23:33:27 +0200

    Initial commit

---
+++ LeanRedis.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `LeanRedis` library.
+-- Import modules here that should be built as part of the library.
+import LeanRedis.Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import LeanRedis
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
